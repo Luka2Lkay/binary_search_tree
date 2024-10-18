cd447ca2c96e9d48102a1cdd23a43cd1fc019f5a
Luka <lukhanyo95@gmail.com>
2024-10-18 11:26:13 +0200

    create buildTree

--- bst.js
+++ bst.js
@@ -7,20 +7,28 @@ class Node {
 }
 
 class Tree {
-    constructor(array){
-        this.sortedArr = array.sort((curr, acc) => curr - acc, 0)
-        this.root = this.buildTree(this.sortedArr);
-    }
+  constructor(array) {
+    this.sortedArr = array.sort((curr, acc) => curr - acc, 0);
+    this.root = this.buildTree(this.sortedArr);
+  }
 
-    buildTree(array){
-return array;
-    }
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const end = array.length - 1;
+
+    const mid = Math.floor(end / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
 }
 
-const arr = [4,3,1,2]
+const arr = [4, 3, 1, 2];
 
 const tree = new Tree(arr);
 
-console.log(tree.buildTree(arr))
-
-
+console.log(tree.buildTree(arr));
